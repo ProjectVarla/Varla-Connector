17c6b5310448af13f4afffd035ec9d9eab116f93
Mohammad Rimawi <mohrimawiz@gmail.com>
2022-12-28 15:36:36 +0300

    Updating Settings

--- gateway/conf.py
+++ gateway/conf.py
@@ -1,12 +1,20 @@
-import os
+from os import getenv
 
+from dotenv import load_dotenv
 from pydantic import BaseSettings
 
+load_dotenv()
+
+TASKS_SERVICE_URL = getenv('TASKS_SERVICE_URL')
+BACKUP_SERVICE_URL = getenv('BACKUP_SERVICE_URL')
+NOTIFICATION_CORE_URL = getenv('NOTIFICATION_CORE_URL')
 
 class Settings(BaseSettings):
     ACCESS_TOKEN_DEFAULT_EXPIRE_MINUTES: int = 360
-    USERS_SERVICE_URL: str = os.environ.get('USERS_SERVICE_URL')
-    ORDERS_SERVICE_URL: str = os.environ.get('ORDERS_SERVICE_URL')
+    TASKS_SERVICE_URL: str = str(TASKS_SERVICE_URL)
+    BACKUP_SERVICE_URL:str = str(BACKUP_SERVICE_URL)
+    NOTIFICATION_CORE_URL:str = str(NOTIFICATION_CORE_URL)
+
     GATEWAY_TIMEOUT: int = 59
 
 
