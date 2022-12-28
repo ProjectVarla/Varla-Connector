6aed9a4413a5dda01c63a1a56bb54de426f02754
Mohammad Rimawi <mohrimawiz@gmail.com>
2022-12-28 23:10:29 +0300

    Moving Port number to Environmental variables

--- bin/main.py
+++ bin/main.py
@@ -1,14 +1,18 @@
+from os import getenv
+
 import uvicorn
 from Cores import Push_Notififcation
+from dotenv import load_dotenv
 from fastapi import FastAPI
 from Services import BackupInterface, TaskInterface
 
 app = FastAPI(title="Varla-Connector")
 
+PORT: int = int(getenv("GATEWAY_PORT"))
 
 TaskInterface(app, ["Tasks"])
 BackupInterface(app, ["Backup"])
 Push_Notififcation(app, ["Notification"])
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", port=8002, reload=True)
+    uvicorn.run("main:app", port=PORT)
