c22ab13a848d003e5e2e3c8dd9dcd7e168bc8721
Mohammad Rimawi <mohrimawiz@gmail.com>
2023-01-01 17:55:23 +0300

    Adding conf file to ProjectVarla/Varla-Connector

---
+++ bin/conf.py
@@ -0,0 +1,46 @@
+from ipaddress import IPv4Address
+from os import getenv
+from typing import Optional
+
+from dotenv import load_dotenv
+from pydantic import BaseSettings, validator
+
+load_dotenv()
+
+
+class Settings(BaseSettings):
+    APP_NAME: str = "VARLA-CONNECTOR"
+
+    ACCESS_TOKEN_DEFAULT_EXPIRE_MINUTES: int = 360
+    GATEWAY_TIMEOUT: int = 59
+
+    BACKUP_SERVICE_URL: Optional[str]
+    TASKS_SERVICE_URL: Optional[str]
+    NOTIFICATION_CORE_URL: Optional[str]
+    DEFAULT_CHANNEL: Optional[str] = "FileManager"
+
+    GATEWAY_HOST: str
+    GATEWAY_PORT: int
+
+    @validator("GATEWAY_PORT", always=True)
+    def gateway_port_validator(cls, v):
+        return int(getenv("GATEWAY_PORT"))
+
+    @validator("GATEWAY_HOST", always=True)
+    def gateway_host_validator(cls, v):
+        return str(IPv4Address(getenv("GATEWAY_HOST")))
+
+    @validator("BACKUP_SERVICE_URL", always=True)
+    def backup_service_url_validator(cls, v):
+        return getenv("BACKUP_SERVICE_URL")
+
+    @validator("TASKS_SERVICE_URL", always=True)
+    def tasks_service_url_validator(cls, v):
+        return getenv("TASKS_SERVICE_URL")
+
+    @validator("NOTIFICATION_CORE_URL", always=True)
+    def notification_core_url_validator(cls, v):
+        return getenv("NOTIFICATION_CORE_URL")
+
+
+settings = Settings()
--- bin/main.py
+++ bin/main.py
@@ -1,19 +1,22 @@
-from os import getenv
-
 import uvicorn
+from conf import settings
 from Cores import Push_Notififcation
-from dotenv import load_dotenv
 from fastapi import FastAPI
 from Services import BackupInterface, TaskInterface
+from VarlaLib.Shell import varla_header
 
 app = FastAPI(title="Varla-Connector")
 
-load_dotenv()
-PORT: int = int(getenv("GATEWAY_PORT"))
 
 TaskInterface(app, ["Tasks"])
 BackupInterface(app, ["Backup"])
 Push_Notififcation(app, ["Notification"])
 
+
 if __name__ == "__main__":
-    uvicorn.run("main:app", port=PORT)
+    varla_header()
+    uvicorn.run(
+        "main:app",
+        port=settings.GATEWAY_PORT,
+        host=str(settings.GATEWAY_HOST),
+    )
