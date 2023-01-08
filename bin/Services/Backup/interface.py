f4e0f3144d69fbcafda879274444ad3fb169206e
Mohammad Rimawi <mohrimawiz@gmail.com>
2023-01-08 19:05:44 +0100

    Adding list functionality to backup service

--- bin/Services/Backup/interface.py
+++ bin/Services/Backup/interface.py
@@ -28,3 +28,16 @@ def Backups_Trigger(app: FastAPI, tags: list[str] = []):
     )
     async def trigger_all_backups(request: Request, response: Response):
         pass
+
+    @route(
+        path="/FileManager/backup/list",
+        request_method=app.post,
+        payload_key=None,
+        status_code=status.HTTP_200_OK,
+        service_url=settings.BACKUP_SERVICE_URL,
+        authentication_required=False,
+        post_processing_func=None,
+        tags=tags,
+    )
+    async def list_backups(request: Request, response: Response):
+        pass
